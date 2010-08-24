5d63411a8c9fd16ec7c1c519db6faa870f89d313
Sam Aaron <samaaron@gmail.com>
2010-08-24 11:14:33 +0200

    update deps to Clojure 1.2.0 stable

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject vijual "0.2.0-SNAPSHOT"
   :description "A Graph Layout Library For Clojure"
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
                      [swank-clojure "1.2.1"]]
   :repositories [["clojars" "http://clojars.org/repo"]])
