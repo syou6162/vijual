e74a555fa245789a80225fccfada886ebc1e6eac
Thomas Karolski <Thomas.Karolski@googlemail.com>
2011-06-23 21:05:33 +0200

    - fix for clojure 1.3

--- project.clj
+++ project.clj
@@ -1,7 +1,5 @@
 (defproject vijual "0.2.0-SNAPSHOT"
   :description "A Graph Layout Library For Clojure"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
-                     [swank-clojure "1.2.1"]]
-  :repositories {"clojars" "http://clojars.org/repo"})
+  :dependencies [[org.clojure/clojure "1.3.0-beta1"] 
+                 [org.clojure.contrib/seq "1.3.0-alpha4"]
+                 [org.clojure.contrib/math "1.3.0-alpha4"]])
