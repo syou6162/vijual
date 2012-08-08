bfd5187dfee76c5edaacada9e6978e62a5340793
Yasuhisa Yoshida <syou6162@gmail.com>
2012-08-08 22:12:56 +0900

    被らないようにグループ名を変更

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject vijual "0.2.0-SNAPSHOT"
+(defproject info.yasuhisay/vijual "0.2.0-SNAPSHOT"
   :description "A Graph Layout Library For Clojure"
   :dependencies [[org.clojure/clojure "1.3.0"]
 		 [org.clojure/clojure-contrib "1.2.0"]])
