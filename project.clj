9489199230c2298032bc1a018cd1edf2bb920635
Yasuhisa Yoshida <syou6162@gmail.com>
2012-08-08 17:47:18 +0900

    依存しているバージョンを変更(clojarsから取ってこれないものが指定されていた)

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
 (defproject vijual "0.2.0-SNAPSHOT"
   :description "A Graph Layout Library For Clojure"
-  :dependencies [[org.clojure/clojure "1.3.0-beta1"] 
-                 [org.clojure.contrib/seq "1.3.0-alpha4"]
-                 [org.clojure.contrib/math "1.3.0-alpha4"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+		 [org.clojure/clojure-contrib "1.2.0"]])
